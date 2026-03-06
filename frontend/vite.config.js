15d6bb0c510b3daa6107083de1690f3c77b9ee7b
Harel Silis <harel.silis@gmail.com>
2026-03-06 11:26:56 +0200

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -36,6 +36,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallbackDenylist: [/^\/sitemap\.xml$/, /^\/robots\.txt$/],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
